61677867413c498859e59c13a5d66d672a6cc5ab
diamante_simone_crescenzo <s295783@studenti.polito.it>
2021-11-30 23:43:29 +0100

    feat: exercise 1 implemented

--- hello_world.c
+++ hello_world.c
@@ -3,11 +3,17 @@
 #include "sys/alt_timestamp.h"
 #include "altera_avalon_pio_regs.h"
 
-#define RXDATA 0x08001060
-#define TXDATA 0x08001064
-#define STATUS 0x08001068
-#define CONTROL 0x0800106C
-#define DIVISOR 0x08001070
+#define DEBUG 1
+#define PROJECT3_1
+
+#define BASE_ADDR 0x08001060
+#define RXDATA_OFFS 0
+#define TXDATA_OFFS 4
+#define STATUS_OFFS 8
+#define CONTROL_OFFS 12
+#define DIVISOR_OFFS 16
+
+#define BAUDRATE 2400
 
 /**
  * Useful functions
@@ -17,9 +23,31 @@
   IOWR_ALTERA_AVALON_PIO_DATA(NIOS_HEADER_CONN_BASE, value);
 */
 
+#ifdef PROJECT3_1
 int main()
 {
-  printf("Hello from Nios II!\n");
+	int divisor_val = 0;
+	int *reg_ptr = (int *)BASE_ADDR;
+	int char_to_send = 83; // 'S'
 
-  return 0;
+	// welcome message
+	printf("NIOSII_UART_DRIVER_project3\n\n");
+
+	// DIVISOR computation for desired BAUDRATE
+	divisor_val = (alt_timestamp_freq()/BAUDRATE)-1;
+#if DEBUG
+	printf("divisor_val:: %d\n", divisor_val);
+#endif
+
+	// writes computed divisor_val into DIVISOR peripheral register
+	*(reg_ptr + DIVISOR_OFFS) = divisor_val;
+
+	// writes char to be sent into TXDATA register
+	printf("\n\n*** Printing char %c ***\n\n", (char)char_to_send);
+	printf("STATUS register before tx:: %d\n", *(reg_ptr + STATUS_OFFS));
+	*(reg_ptr + TXDATA_OFFS) = char_to_send;
+	printf("STATUS register after tx:: %d\n", *(reg_ptr + STATUS_OFFS));
+
+	return 0;
 }
+#endif
