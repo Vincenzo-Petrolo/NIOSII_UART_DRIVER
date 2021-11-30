8fc66d837199d504c4a72df8299b50771b609345
Vincenzo Petrolo <vincenzo@kernel-space.org>
2021-11-30 15:29:36 +0100

    chore : added useful things in base project

--- hello_world.c
+++ hello_world.c
@@ -1,20 +1,21 @@
-/*
- * "Hello World" example.
- *
- * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
- * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example
- * designs. It runs with or without the MicroC/OS-II RTOS and requires a STDOUT
- * device in your system's hardware.
- * The memory footprint of this hosted application is ~69 kbytes by default
- * using the standard reference design.
- *
- * For a reduced footprint version of this template, and an explanation of how
- * to reduce the memory footprint for a given application, see the
- * "small_hello_world" template.
- *
- */
-
 #include <stdio.h>
+#include "system.h"
+#include "sys/alt_timestamp.h"
+#include "altera_avalon_pio_regs.h"
+
+#define RXDATA 0x08001060
+#define TXDATA 0x08001064
+#define STATUS 0x08001068
+#define CONTROL 0x0800106C
+#define DIVISOR 0x08001070
+
+/**
+ * Useful functions
+  int alt_timestamp_freq(void)
+  void alt_timestamp_start(void)
+  int alt_timestamp(void)
+  IOWR_ALTERA_AVALON_PIO_DATA(NIOS_HEADER_CONN_BASE, value);
+*/
 
 int main()
 {
