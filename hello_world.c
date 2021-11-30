69e3e358ec4027d9a1825bccb6a9192c21457995
diamante_simone_crescenzo <s295783@studenti.polito.it>
2021-12-01 00:10:54 +0100

    fix: got skeleton ready

--- hello_world.c
+++ hello_world.c
@@ -3,11 +3,18 @@
 #include "sys/alt_timestamp.h"
 #include "altera_avalon_pio_regs.h"
 
-#define RXDATA 0x08001060
-#define TXDATA 0x08001064
-#define STATUS 0x08001068
-#define CONTROL 0x0800106C
-#define DIVISOR 0x08001070
+#define DEBUG 1
+#define EVER ;;
+#define PROJECT4_1
+
+#define BASE_ADDR 0x08001060
+#define RXDATA_OFFS 0
+#define TXDATA_OFFS 4
+#define STATUS_OFFS 8
+#define CONTROL_OFFS 12
+#define DIVISOR_OFFS 16
+
+#define BAUDRATE 2400
 
 /**
  * Useful functions
@@ -17,9 +24,24 @@
   IOWR_ALTERA_AVALON_PIO_DATA(NIOS_HEADER_CONN_BASE, value);
 */
 
+#ifdef PROJECT4_1
 int main()
 {
-  printf("Hello from Nios II!\n");
+	int divisor_val = 0;
+		int *reg_ptr = (int *)BASE_ADDR;
+		int char_to_send = 83; // 'S'
 
-  return 0;
+		// welcome message
+		printf("NIOSII_UART_DRIVER_project4\n\n");
+
+		// DIVISOR computation for desired BAUDRATE
+		divisor_val = (alt_timestamp_freq()/BAUDRATE)-1;
+	#if DEBUG
+		printf("divisor_val:: %d\n", divisor_val);
+	#endif
+
+	for(EVER);
+
+	return 0;
 }
+#endif
