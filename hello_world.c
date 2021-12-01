729242f3ae6d87aff45005a302a226d4a92a64c9
AlfredoPaolino <paolinoalfre@gmail.com>
2021-12-01 19:51:40 +0100

    added while(1)

--- hello_world.c
+++ hello_world.c
@@ -1,31 +1,13 @@
-/*
- * "Hello World" example.
- *
- * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
- * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example
- * designs. It runs with or without the MicroC/OS-II RTOS and requires a STDOUT
- * device in your system's hardware.
- * The memory footprint of this hosted application is ~69 kbytes by default
- * using the standard reference design.
- *
- * For a reduced footprint version of this template, and an explanation of how
- * to reduce the memory footprint for a given application, see the
- * "small_hello_world" template.
- *
- */
-
-#include <stdio.h>
-#include "system.h"
-#include "sys/alt_timestamp.h"
-
 int main()
 {
 	int *base_address = 0x08001060;
+	int reg;
 	for ( int i = 0 ; i < 5; i++)
 	{
-		int val = *base_address;
+		reg = *UARTRX;
 		printf ( "%d\n", val);
 		base_address += 4;
 	}
+	while(1);
 	return 0;
 }
