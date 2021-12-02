b477e2f3c0562545dfaccdd2efb86d2c5e96381d
AlfredoPaolino <paolinoalfre@gmail.com>
2021-12-02 19:13:51 +0100

    added libraries

--- hello_world.c
+++ hello_world.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include "system.h"
 #include "sys/alt_timestamp.h"
+#include "uartlib.h"
 
 #DEFINE BR1 115200
 #DEFINE BR2 2400
 
-#define RXDATA 0x08001060
-#define TXDATA 0x08001064
-#define STATUS 0x08001068
-#define CONTROL 0x0800106C
-#define DIVISOR 0x08001070
-
 int main()
 {
 	int freq = alt_timestamp_freq(void);
-	int divisor1 = freq/BR1 - 1;
-	int divisor2 = freq/BR2 - 1;
+	int divisor1 = (alt_timestamp_freq(void)/BR1) - 1;
+	int divisor2 = (alt_timestamp_freq(void)/BR2) - 1;
 	int *pdiv = (int *) DIVISOR;
 	*pdiv = divisor2;
 	printf("%d", *pdiv);
