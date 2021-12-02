e071851126ea932962ade0cc30a85fe960296e67
diamante_simone_crescenzo <s295783@studenti.polito.it>
2021-12-02 19:32:40 +0100

    chor: code simplified

--- project11.c
+++ project11.c
@@ -11,16 +11,23 @@ int main(void)
 	char recChar;
 	char string[] = "You are now elegant. Well Done!\n";
 
+	// opens communication with UART peripheral through drivers
 	fp = fopen("/dev/uart_0", "r+");
+	// if communication fails, throws an error
 	if(fp == NULL){
 		fprintf(stderr, "Failed to open /dev/uart_0\n");
 		return 1;
 	}
 
-	for(int c=0; c<strlen(string)-1; c++){
+	/*for(int c=0; c<strlen(string)-1; c++){
 		fprintf(fp, "%c", string[c]);
-	}
+	}*/
 
+	// sends a string on the line through UART peripheral
+	fprintf(fp, "%s", string);
+
+	// endless loop in which UART peripheral will receive
+	//		data from the COM line
     for (EVER){
     	recChar = fgetc(fp);
     	printf("%c", recChar);
